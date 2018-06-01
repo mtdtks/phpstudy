fbf7cc4aa6d09fa88a0cb9c1e83b52e954fda1b6
t-matsuda <t-matsuda@keyportsolutions.com>
2018-06-01 09:37:21 +0900

    initial commit.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: t-matsuda
+ * Date: 2018/06/01
+ * Time: 9:21
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title></title>
+<meta charset="utf-8">
+<!-- <link rel="stylesheet" href=""> -->
+</head>
+<body>
+<?php
+echo '$_SERVER';
+var_dump($_SERVER);
+echo '<BR>';
+var_dump($_GET);
+?>
+</body>
+</html>
